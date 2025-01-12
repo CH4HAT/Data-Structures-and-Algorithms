fcaa9bc0520bb7155c19e875a21fcfba3576d5bd
chahat <cchahat@my.bcit.ca>
2025-01-12 12:06:20 -0800

    Binary search for a finite sorted array

--- Week1/algolab1.py
+++ Week1/algolab1.py
@@ -1,5 +1,5 @@
-# Linear Search
-
+# Part 1 Linear Search
+'''
 def linear_search(needle, haystack):
     count=0
     for i in haystack:
@@ -8,6 +8,7 @@ def linear_search(needle, haystack):
             return print(count)
 
 linear_search(8, [6,2,8,4])
+'''
 # Part 2 - Binary Search
 '''
 def binary_search(needle,haystack):
@@ -36,3 +37,32 @@ if result != -1:
 else:
 	print("Element is not present in array")
 '''
+
+# Part 2 
+
+def binary_search(needle,haystack):
+    high = len(haystack)-1
+    mid = 0
+    low = 0
+    while low <=high:
+        mid = (high + low) //2
+        if haystack[mid] < needle:
+            low = mid + 1
+        elif haystack[mid] > needle:
+            high = mid - 1
+        else:
+            return mid
+    return 0
+
+prob1 = binary_search(8, [2, 4, 6, 8])    
+prob2 = binary_search(103, [2, 4, 6, 8, 98, 99, 100, 101, 102, 103, 104]) 
+
+if prob1 == 0:
+    print("not present")
+else:
+    print(prob1)
+    
+if prob2 == 0:
+    print("not present")
+else:
+    print(prob2)
