e87d02f1a742817f6e79bd188b7b45c20022ccf3
chahat <cchahat@my.bcit.ca>
2025-01-14 23:20:00 -0800

    Climbing stairs Solution added

--- Leetcode/leetcode.py
+++ Leetcode/leetcode.py
@@ -9,3 +9,15 @@ class Solution:
         return self.step(n-1)+ self.step(n-2)
     def climbStairs(self, n: int) -> int:
         return self.step(n)
+
+# 0ms Runtime
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a = 1
+        b = 0
+        for i in range(1, n+1):
+            c = a + b
+            b = a
+            a = c
+        return a 
